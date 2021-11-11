80ec7bf96674388ad3a2f7220e292bd9881750d8
Martin Kruchinski <martin.kruchinskialmeida@mail.mcgill.ca>
2021-11-10 23:17:45 -0400

    Finished everything

--- LAB 2/part3.s
+++ LAB 2/part3.s
@@ -3,6 +3,13 @@ PB_int_flag :
 	
 tim_int_flag :
     .word 0x0
+	
+ctrHex_2: .word 0x0
+ctrHex_3: .word 0x0
+ctrHex_4: .word 0x0
+ctrHex_5: .word 0x0
+
+
 .section .vectors, "ax"
 B _start
 B SERVICE_UND       // undefined instruction vector
@@ -20,7 +27,18 @@ B SERVICE_FIQ       // FIQ interrupt vector
 .equ loadTimer, 0xFFFEC600
 .equ MaskBits, 0xFF200058
 .equ EdgeBits, 0xFF20005C
+
+
+.equ hex0, 0xFF200020
+.equ hex1, 0xFF200021
+.equ hex2, 0xFF200022
+.equ hex3, 0xFF200023
+.equ hex4, 0xFF200030
+.equ hex5, 0xFF200031
+
 initialCount: .word 2000000 // 10 milisecond
+floodzeros: .word 0x3F3F3F3F//zeros
+
 
 _start:
     /* Set up stack pointers for IRQ and SVC processor modes */
@@ -44,39 +62,37 @@ _start:
     // enable IRQ interrupts in the processor
     MOV        R0, #0b01010011      // IRQ unmasked, MODE = SVC
     MSR        CPSR_c, R0
-	ldr r1, =HEX_3to0
-	mov r6, #0 //initialize count
-	mov r10, #0
-	bl HEX_write_ASM
-	mov r10, #0x1
-	bl HEX_write_ASM
-	mov r10, #0x2
-	bl HEX_write_ASM
-	mov r10, #0x3
-	bl HEX_write_ASM
-	mov r10, #0x10
-	bl HEX_write_ASM
-	mov r10, #0x11
-	bl HEX_write_ASM
-	mov r10, #0x0
-	mov r8, #0x0
-	mov r9, #0x0
-	mov r11, #0x0
-	mov r12, #0x0
-	mov r6, #0
 	
+	//
+	bl HEX_flood_ASM
+	mov r2, #0 //COUNTER FOR HEX 0
+	mov r3, #0 //counter for hex 1
 
 IDLE:
+	ldr r1, =tim_int_flag
+	ldr r0, [r1]
+	push {r9}
+	mov r9, #0
+	cmp r0, #1
+	streq r9, [r1]
+	pop {r9}
+	blt IDLE
+	
 	ldr r0, =PB_int_flag
 	ldr r1, [r0]
+	
 	cmp r1, #0b001
-	beq display_0
+	bleq main_loop
+	
 	cmp r1, #0b011
 	bleq PB_clear_edgecp_ASM
+	
 	cmp r1, #0b100
 	bleq PB_clear_edgecp_ASM
 	beq _start
+	
     B IDLE 
+	
 	
 
 /*--- Undefined instructions ---------------------------------------- */
@@ -245,240 +261,207 @@ PB_clear_edgecp_ASM:
 	bx lr
 	
 
-display_0:
-	ldr r1, =tim_int_flag
-	ldr r0, [r1]
-	mov r3, #0
-	cmp r0, #1
-	streq r3, [r1]
-	blt display_0
-	bleq HEX_write_ASM
-	add r6, r6, #1
-	bl ARM_TIM_clear_INT_ASM
-	cmp r6, #10
-	blt display_0
-	add r10, r10, #0x1
-	beq display_1
-	
-display_1:	
-	ldr r1, =tim_int_flag
-	ldr r0, [r1]
-	mov r3, #0
-	cmp r0, #1
-	streq r3, [r1]
-	blt display_1
-	add r7, r7, #1
-	mov r6, r7
-	cmp r7, #10
-	bllt HEX_write_ASM
-	bllt ARM_TIM_clear_INT_ASM
-	mov r10, #0x0
-	mov r6, #0x0
-	cmp r7, #10
-	bllt IDLE
-	mov r7, #0
-	mov r6, r7
+main_loop:
+push {lr}
+//display_0
+	ldr r1, =hex0 //address first display
+	//add r1, r1, #1
 	bl HEX_write_ASM
-	bl ARM_TIM_clear_INT_ASM
-	add r10, r10, #0x2
-	b display_2
+	add r2, r2, #1 //add 1 to counter display 0
+	cmp r2, #10
+	poplt {lr}
+	bxlt lr
 	
-display_2:
-	ldr r1, =tim_int_flag
-	ldr r0, [r1]
-	mov r3, #0
-	cmp r0, #1
-	streq r3, [r1]
-	blt display_2
-	add r8, r8, #1
-	mov r6, r8
-	cmp r8, #10
-	bllt HEX_write_ASM
-	bllt ARM_TIM_clear_INT_ASM
-	mov r10, #0x1
-	mov r6, #0x0
-	cmp r8, #10
-	bllt HEX_write_ASM
-	mov r10, #0x0
-	cmp r8, #10
-	bllt HEX_write_ASM
-	cmp r8, #10
-	blt IDLE 
-	add r10, r10, #0x3
-	b display_3
-display_3:
+	//0 to hex_0
+	push {r10}
+	mov r10, #0x3f
+	strb r10, [r1]
+	pop {r10}
 	
-	ldr r1, =tim_int_flag
-	ldr r0, [r1]
-	mov r3, #0
-	cmp r0, #1
-	streq r3, [r1]
-	blt display_3
-	add r9, r9, #1
-	mov r6, r9
-	//
-	cmp r9, #6
-	bllt HEX_write_ASM
-	bllt ARM_TIM_clear_INT_ASM
-	mov r10, #0x2
-	mov r6, #0x0
-	cmp r9, #6
-	bllt HEX_write_ASM
-	mov r10, #0x1
-	cmp r9, #6
-	bllt HEX_write_ASM
-	mov r10, #0x0
-	cmp r9, #6
-	bllt HEX_write_ASM
-	mov r8, #0
-	cmp r9, #6
-	blt IDLE 
-	mov r10, #0x10
-	b display_4
+//display_1
+	ldr r1, =hex1
+	add r3, r3, #1
+	mov r2, r3
+	bl HEX_write_ASM
+	mov r2, #0 //reset counter hex0 to 0
+	cmp r3, #10
+	poplt {lr}
+	bxlt lr
 	
-display_4:
-	ldr r1, =tim_int_flag
-	ldr r0, [r1]
-	mov r3, #0
-	cmp r0, #1
-	streq r3, [r1]
-	blt display_3
-	add r11, r11, #1
-	mov r6, r11
-	cmp r11, #10
-	bllt HEX_write_ASM
-	bllt ARM_TIM_clear_INT_ASM
-	mov r10, #0x3
-	mov r6, #0x0
-	cmp r11, #10
-	bllt HEX_write_ASM
-	mov r10, #0x2
-	cmp r11, #10
-	bllt HEX_write_ASM
-	mov r10, #0x1
-	cmp r11, #10
-	bllt HEX_write_ASM
-	mov r10, #0x0
-	cmp r11, #10
-	bllt HEX_write_ASM
-	mov r8, #0
-	mov r9, #0
-	cmp r11, #10
-	blt IDLE //change to link?
-	mov r10, #0x11
-	b display_5
+	//0 to hex_1
+	push {r10}
+	mov r10, #0x3f
+	strb r10, [r1]
+	pop {r10}
+	mov r3, #0 //reset counter hex_1
 	
-display_5:
-	ldr r1, =tim_int_flag
-	ldr r0, [r1]
-	mov r3, #0
-	cmp r0, #1
-	streq r3, [r1]
-	blt display_3
-	add r12, r12, #1
-	mov r6, r12
-	cmp r12, #6
-	bllt HEX_write_ASM
-	bllt ARM_TIM_clear_INT_ASM
-	mov r10, #0x10
-	mov r6, #0x0
-	cmp r12, #6
-	bllt HEX_write_ASM
-	mov r10, #0x3
-	cmp r12, #6
-	bllt HEX_write_ASM
-	mov r10, #0x2
-	cmp r12, #6
-	bllt HEX_write_ASM
-	mov r10, #0x1
-	cmp r12, #6
-	bllt HEX_write_ASM
-	mov r10, #0x0
-	cmp r12, #6
-	bllt HEX_write_ASM
-	mov r8, #0
-	mov r9, #0
-	mov r11, #0
-	cmp r12, #6
-	blt IDLE //change to link?
-	b end
+//display_2
+	ldr r1, =hex2
+	push {r4, r10}
+	ldr r10, =ctrHex_2 //get counter hex_2
+	ldr r4, [r10] //get value counter hex_2
+	add r4, r4, #1 //add 1 to counter hex_2
+	str r4, [r10] //store new value to counter hex_2
+	mov r2, r4
+	bl HEX_write_ASM
+	mov r2, #0 //reset counter hex0 to 0
+	cmp r4, #10
+	pop {r4, r10}
+	poplt {lr}
+	bxlt lr
 	
-ARM_TIM_clear_INT_ASM:
-	mov r4, #0x00000001
-	strb r4, [r2, #0xC]
-	bx lr
+	//0 to hex_1
+	push {r4, r10}
+	mov r10, #0x3f
+	strb r10, [r1]
+	ldr r10, =ctrHex_2 //get counter hex_2
+	mov r4, #0
+	str r4, [r10] //get value counter hex_2
+	pop {r4, r10}
+
+//display_3
+	ldr r1, =hex3
+	push {r4, r10}
+	ldr r10, =ctrHex_3 //get counter hex_2
+	ldr r4, [r10] //get value counter hex_2
+	add r4, r4, #1 //add 1 to counter hex_2
+	str r4, [r10] //store new value to counter hex_2
+	mov r2, r4
+	bl HEX_write_ASM
+	mov r2, #0 //reset counter hex0 to 0
+	cmp r4, #6
+	pop {r4, r10}
+	poplt {lr}
+	bxlt lr
+	
+	//0 to hex_1
+	push {r4, r10}
+	mov r10, #0x3f
+	strb r10, [r1]
+	ldr r10, =ctrHex_3 //get counter hex_2
+	mov r4, #0
+	str r4, [r10] //get value counter hex_2
+	pop {r4, r10}
+
+//display_4
+	ldr r1, =hex4
+	push {r6, r10}
+	ldr r10, =ctrHex_4 //get counter hex_2
+	ldr r6, [r10] //get value counter hex_2
+	add r6, r6, #1 //add 1 to counter hex_2
+	str r6, [r10] //store new value to counter hex_2
+	mov r2, r6
+	bl HEX_write_ASM
+	mov r2, #0 //reset counter hex0 to 0
+	cmp r6, #10
+	pop {r6, r10}
+	poplt {lr}
+	bxlt lr
+	
+	//0 to hex_1
+	push {r6, r10}
+	mov r10, #0x3f
+	strb r10, [r1]
+	ldr r10, =ctrHex_4 //get counter hex_2
+	mov r6, #0
+	str r6, [r10] //get value counter hex_2
+	pop {r6, r10}
+
+//display_5
+	ldr r1, =hex5
+	push {r6, r10}
+	ldr r10, =ctrHex_5 //get counter hex_2
+	ldr r6, [r10] //get value counter hex_2
+	add r6, r6, #1 //add 1 to counter hex_2
+	str r6, [r10] //store new value to counter hex_2
+	mov r2, r6
+	bl HEX_write_ASM
+	mov r2, #0 //reset counter hex0 to 0
+	cmp r6, #6
+	pop {r6, r10}
+	poplt {lr}
+	bxlt lr
+	b _start
 
 HEX_write_ASM:
-	ldr r1, =HEX_3to0
-	b get_hexs
-
-get_hexs: //get hexadecimal value
 	push {r5}
-	cmp r6, #0
+	
+	cmp r2, #0
 	moveq r5, #0b0111111 //encoding number 0 binary
 	beq next
 
-	cmp r6, #1
+	cmp r2, #1
 	moveq r5, #0b0000110 //encoding number 1 binary
 	beq next
 	
-	cmp r6, #2
+	cmp r2, #2
 	moveq r5, #0b1011011 //encoding number 2 binary
 	beq next
 	
-	cmp r6, #3
+	cmp r2, #3
 	moveq r5, #0b1001111 //encoding number 3 binary
 	beq next
 	
-	cmp r6, #4
+	cmp r2, #4
 	moveq r5, #0b1100110 //encoding number 4 binary
 	beq next
 	
-	cmp r6, #5
+	cmp r2, #5
 	moveq r5, #0b1101101 //encoding number 5 binary
 	beq next
 	
-	cmp r6, #6
+	cmp r2, #6
 	moveq r5, #0b1111101 //encoding number 6 binary
 	beq next
 	
-	cmp r6, #7
+	cmp r2, #7
 	moveq r5, #0b0000111 //encoding number 7 binary
 	beq next
 	
-	cmp r6, #8
+	cmp r2, #8
 	moveq r5, #0b1111111 //encoding number 8 binary
 	beq next
 	
-	cmp r6, #9
+	cmp r2, #9
 	moveq r5, #0b1100111 //encoding number 9 binary
 	beq next
 	
-	cmp r6, #10
+	cmp r2, #10
 	moveq r5, #0b1110111 //encoding number A binary
 	beq next
 	
-	cmp r6, #11
+	cmp r2, #11
 	moveq r5, #0b1111100 //encoding number B binary
 	beq next
 	
-	cmp r6, #12
+	cmp r2, #12
 	moveq r5, #0b0111001 //encoding number C binary
 	beq next
 	
-	cmp r6, #13
+	cmp r2, #13
 	moveq r5, #0b1011110 //encoding number D binary
 	beq next
 	
-	cmp r6, #14
+	cmp r2, #14
 	moveq r5, #0b1111001 //encoding number E binary
 	beq next
 	
-	cmp r6, #15
+	cmp r2, #15
 	moveq r5, #0b1110001 //encoding number F binary
 	beq next
 	
 next:
-	strb r5, [r1, r10]
+	strb r5, [r1] //write number
+	pop {r5}
+	bx lr
+	
+	
+HEX_flood_ASM:
+	push {r5}
+	ldr r1, =0xFF200020
+	ldr r5, floodzeros
+	str r5, [r1]
+	ldr r1, =0xFF200030
+	str r5, [r1]
 	pop {r5}
 	bx lr
